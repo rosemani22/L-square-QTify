69ae6f0ae3d366735a886c08609bf94bc3306b11
rosemani22 <rosemani.neemi@gmail.com>
2024-07-23 00:45:27 +0530

    Test commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,24 +1,20 @@
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import StyledEngineProvider from "@mui/material/StyledEngineProvider";
+import Navbar from "./components/Navbar/Navbar.jsx";
+//import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 import './App.css';
 //import TopAlbums from "./components/TopAlbums/TopAlbums";
 //import NewAlbums from "./components/NewAlbums/NewAlbums";
- import Hero from "./components/Hero/Hero";
+ import Hero from "./components/Hero/Hero.jsx";
  //import Songs from "./components/Songs/Songs";
 
 
 
 function App() {
   return(
-  <StyledEngineProvider injectFirst>
+    <div className="App">
     <Navbar />
-    <Hero/>
-    {/* <TopAlbums/>
-    <NewAlbums/>
-    <Songs/> */}
-  </StyledEngineProvider>
-      
+    <Hero />
+    </div>
    
   );
 }
