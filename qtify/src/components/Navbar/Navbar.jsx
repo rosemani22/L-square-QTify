5b9074b86723cd0016a597a59f1658e944357ff3
rosemani22 <rosemani.neemi@gmail.com>
2024-07-22 21:05:50 +0530

    Test commit

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../Button/Button";
+import Button from "./Button/Button";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import styles from "./Navbar.module.css";
